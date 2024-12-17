55e3c4beea922db3739ff03807b57b6869b0be57
zxcqwe <0194066@gmail.com>
2024-12-17 22:01:41 +0300

    починил handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/hwtdspprcmpltl/calcyandex/internal/calc"
 )
@@ -17,23 +18,36 @@ type HTTPAnswer struct {
 }
 
 func HandleCalculator(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		answer := HTTPAnswer{Error: "разрешен только пост запрос :<"}
+		json.NewEncoder(w).Encode(answer)
+	}
+
 	var request PostRequest
 
-	err := json.NewDecoder(r.Body).Decode(&request)
-	if err != nil {
-		w.WriteHeader(422) //не так с входными(декод)
-		answer := HTTPAnswer{Error: "неправильный ввод (ошибка с декодером)"}
-		json.NewEncoder(w).Encode(answer)
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		json.NewEncoder(w).Encode(HTTPAnswer{Error: "неправильный тип ввода (не декодается)"}) //поправь
 		return
 	}
 
 	result, err := calc.Calculate(string(request.Expression))
 	if err != nil {
-		w.WriteHeader(500) //здесь надо все ошибки задебагить
-		return
+		if strings.Contains(err.Error(), "invalid") {
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			answer := HTTPAnswer{Error: "неправильное выражение"}
+			json.NewEncoder(w).Encode(answer)
+			return
+		} else {
+			answer := HTTPAnswer{Error: "ошибка сервера"}
+			json.NewEncoder(w).Encode(answer)
+			return
+		}
 	}
-
-	w.WriteHeader(200)
+	w.WriteHeader(http.StatusOK)
 	answer := HTTPAnswer{Result: result}
 	json.NewEncoder(w).Encode(answer)
 
